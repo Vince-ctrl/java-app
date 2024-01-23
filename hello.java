deddb47637e000b5db356b07327126a6e8389961
Vincent Adams <61333244+Vince-ctrl@users.noreply.github.com>
2024-01-23 12:41:56 -0700

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,17 @@
+/**
+ * hello
+ */
+public class hello {
+
+     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String name=("My name is Vincent");
+        String age=("I am 15");
+        String parentNames=("My dad's name is Michael, and my mom's Audrey");
+        String sisterNames=("My sister's names are Evelyn and Ally");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(parentNames);
+        System.out.println(sisterNames);
+    }
+}
